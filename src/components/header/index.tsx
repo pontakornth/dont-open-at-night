4cb9488e6fbc9d84642fcd976a9f1f10f6223afb
pontakornth <pontakorn61@gmail.com>
2021-02-01 21:53:07 +0700

    init: Initialize repository

---
+++ src/components/header/index.tsx
@@ -0,0 +1,9 @@
+import { FunctionalComponent, h } from "preact";
+// import { Link } from "preact-router/match";
+// import * as style from "./style.css";
+
+const Header: FunctionalComponent = () => {
+    return <header>This is header for now</header>;
+};
+
+export default Header;
