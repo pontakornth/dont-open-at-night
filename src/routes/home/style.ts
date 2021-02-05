bf382d1ef18dc506594807b570ff071ee67c80aa
pontakornth <pontakorn61@gmail.com>
2021-02-05 19:48:00 +0700

    styling: Add width to container

--- src/routes/home/style.ts
+++ src/routes/home/style.ts
@@ -3,5 +3,8 @@ import { IStyle } from "fela";
 export const Container = (): IStyle => ({
     display: "flex",
     height: "100%",
-    width: "100%"
+    width: "80%",
+    maxWidth: "1200px",
+    marginLeft: "auto",
+    marginRight: "auto"
 });
