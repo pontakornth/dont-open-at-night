93c4eabe2d9c5bf07a683d5f8a050eada70d4519
pontakornth <pontakorn61@gmail.com>
2021-02-05 20:09:25 +0700

    feat: Add mock horror message to content

--- src/components/content/index.tsx
+++ src/components/content/index.tsx
@@ -1,25 +1,38 @@
-import { combineRules } from "fela";
-import { FunctionalComponent, h } from "preact";
+import { Fragment, FunctionalComponent, h } from "preact";
 import { useState, useLayoutEffect } from "preact/hooks";
 import { useFela } from "preact-fela";
-import lorem from "./lorem";
-import { ContentLayoutStyle, ContentNightStyle } from "./style";
+import HorrorMessage from "../../components/horror-message";
+import { ContentLayoutStyle } from "./style";
 
 const Content: FunctionalComponent = () => {
-    const [message, setMessage] = useState<string>("");
+    const [isNighttime, setNighttime] = useState<boolean>(false);
     useLayoutEffect(() => {
         const now = new Date();
-        if (now.getHours() > 18 || now.getHours() < 5) {
-            setMessage("I think you should not be here.");
-        } else {
-            setMessage(lorem);
+        if (now.getHours() >= 19 || now.getHours() <= 5) {
+            setNighttime(true);
         }
     }, []);
     const { css } = useFela();
     return (
         <div class={css(ContentLayoutStyle)}>
             <h1>Lorem ipsum dolor amend</h1>
-            <p>{message}</p>{" "}
+            {isNighttime ? (
+                <Fragment>
+                    <HorrorMessage>
+                        I have been tracked by someone or something for a while.
+                        I always feel uneasy when I code. It haunt me. It is
+                        always here. For some reasons, I think it&apos;s because
+                        I overwork.
+                    </HorrorMessage>
+                    <HorrorMessage>
+                        But no. I already tried to rest but it&apos;s still
+                        there. there. It&apos;s a shadowy figure. I think
+                        it&apos; look human. It&apos;s height is similar to me.
+                    </HorrorMessage>
+                </Fragment>
+            ) : (
+                "This is daytime."
+            )}
         </div>
     );
 };
